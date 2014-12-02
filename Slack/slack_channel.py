bb70e29c686f177cce5acf8157b4a72fba817fb2
David Nesbitt <david.nesbitt@plexxi.com>
2014-12-02 10:39:18 -0500

    Added slack_api class and extracted message to its own sub-method

--- Slack/slack_channel.py
+++ Slack/slack_channel.py
@@ -5,24 +5,22 @@ import json
 import requests
 import sys
 
+class slack_api:
 
-#Defined the class to use when submitting a post to a slack instance
-class post_to_slack():
-    def __init__(self, channel, username, icon, message, url):
+    def __init__(self, channel, username, icon, url):
 
         self.channel = channel
         self.username = username
         self.icon = icon
-        self.message = message
         self.url = url
-        self.slack_call()
 
-    #Defines the object to use when posting to a slack integration
-    def slack_call(self):
+    def post_to_slack(self, message):
+
+        #Defines the payload to push out the slack channel
         payload = {'channel': self.channel,
                     'username': self.username,
                     'icon_emoji': self.icon,
-                    'text': self.message
+                    'text': message
                     }
 
         return requests.post(self.url, json.dumps(payload))
