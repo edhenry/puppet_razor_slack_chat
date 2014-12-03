d4e5e05b53ed2fa5c9cf08fc858eaf438f697b8b
Edward Henry <ed.henry@plexxi.com>
2014-12-03 13:18:06 -0500

    Added GET methods for more razor API endpoints

--- razor_slack.py
+++ razor_slack.py
@@ -70,7 +70,136 @@ def single_node(ip, port):
     #Post your message to the slack channel
     return slack_channel.post_to_slack(message)
 
+def all_tags(ip, port):
 
-all_nodes('RAZOR SERVER', 'RAZOR SERVER PORT')
+    #Create the Razor server address
+    server = str(ip) + ':' + str(port)
+
+    #Instantiates the razor_api object
+    razor_channel = razor_api(razor_server=server)
+
+    #Pull all the nodes from Razor
+    tags = razor_channel.razor_all_tags()
+
+    #Instantiate list to populate with list of nodes from Razor
+    tag_list = []
+
+    #Parse dictionary and append results to list
+    for t in tags['items']:
+        tag_list.append(str(t['name']))
+
+    #Variable definition for message to post to slack
+    message = "Tags currently configured within Puppet Razor:" + str(tag_list)
+
+    #Define the slack channel to send the message out on
+    slack_channel = slack_api(channel='ed-integration-tests', username='integration-bot-ed', icon='', url=slack_url)
+
+    #Post your message to the slack channel
+    return slack_channel.post_to_slack(message)
+
+def single_tag(ip, port):
+
+    #Create the Razor server address
+    server = str(ip) + ':' + str(port)
+
+    #Instantiates the razor_api object
+    razor_channel = razor_api(razor_server=server)
+
+    #Pull all the nodes from Razor
+    tag = razor_channel.razor_single_tag(tag_name=sys.argv[1])
+
+    #Instantiate list to populate with list of rules for tag from Razor
+    rule_list = []
+
+    #Parse dictionary and append results to list
+    for t in tag['rule']:
+        rule_list.append(t)
+
+    #Create node_list to store list of nodes tag is applied to
+    node_list = []
+
+    #Iterate over tag dict and store nodes result in node list
+    for t in tag['nodes'].iteritems():
+        print t
+
+    #TODO Split the list for formatting when delivering message to slack
+    #Variable definition for message to post to slack
+    message = "The tag *%s* " % sys.argv[1] + "currently has the following rules configured : \n"\
+              "\n"\
+              + str(rule_list)
+
+    #Define the slack channel to send the message out on
+    slack_channel = slack_api(channel='ed-integration-tests', username='integration-bot-ed', icon='', url=slack_url)
+
+    #Post your message to the slack channel
+    return slack_channel.post_to_slack(message)
+
+def all_policies(ip, port):
+
+    #Create the Razor server address
+    server = str(ip) + ':' + str(port)
+
+    #Instantiates the razor_api object
+    razor_channel = razor_api(razor_server=server)
+
+    #Pull all the nodes from Razor
+    policies = razor_channel.razor_all_policies()
+
+    #Instantiate list to populate with list of nodes from Razor
+    tag_list = []
+
+    #Parse dictionary and append results to list
+    for p in policies['items']:
+        tag_list.append(str(p['name']))
+
+    #TODO Formatting of message for posting to Slack
+    #Variable definition for message to post to slack
+    message = "Policies currently configured within Puppet Razor : \n" \
+               + str(tag_list)
+
+    #Define the slack channel to send the message out on
+    slack_channel = slack_api(channel='ed-integration-tests', username='integration-bot-ed', icon='', url=slack_url)
+
+    #Post your message to the slack channel
+    return slack_channel.post_to_slack(message)
+
+def single_policy(ip, port):
+
+    #Create the Razor server address
+    server = str(ip) + ':' + str(port)
+
+    #Instantiates the razor_api object
+    razor_channel = razor_api(razor_server=server)
+
+    #Pull all the nodes from Razor
+    policy = razor_channel.razor_single_policy(policy_name=sys.argv[1])
+
+    #Instantiate list to populate with list of rules for tag from Razor
+    policy_list = []
+
+    #Parse dictionary and append results to list
+    for p in policy['rule']:
+        policy_list.append(p)
+
+        print p
+
+    #TODO Split the list for formatting when delivering message to slack
+    #Variable definition for message to post to slack
+    message = "The tag *%s* " % sys.argv[1] + "currently has the following rules configured : \n"\
+              "\n"\
+              + str(policy_list)
+
+    #Define the slack channel to send the message out on
+    slack_channel = slack_api(channel='ed-integration-tests', username='integration-bot-ed', icon='', url=slack_url)
+
+    #Post your message to the slack channel
+    return slack_channel.post_to_slack(message)
+
+
+#all_nodes('RAZOR SERVER', 'RAZOR SERVER PORT')
 #single_node('RAZOR SERVER', 'RAZOR SERVER PORT')
+#all_tags('RAZOR SERVER', 'RAZOR SERVER PORT')
+#single_tag('RAZOR SERVER', 'RAZOR SERVER PORT')
+#all_policies('RAZOR SERVER', 'RAZOR SERVER PORT')
+#single_policy('RAZOR SERVER', 'RAZOR SERVER PORT')
 
